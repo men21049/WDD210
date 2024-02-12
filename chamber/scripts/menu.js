96df2296f1fca5171d0df393e5a3349a9119968e
men21049 <men21049@byui.edu>
2024-02-11 22:44:00 -0600

    5 Assignment

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,7 +1,31 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
+const changeColor = document.querySelector('.addColor');
+const mainColor = document.getElementById('main');
+const card = document.querySelectorAll(".card");
+const cardGreen = document.querySelectorAll(".cardGreen");
+
+
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+
+changeColor.onclick = function(e){
+	navigation.classList.toggle('navigationDark');
+
+	for(let elem of card){
+		elem.classList.toggle('cardDark');
+	}
+	
+	if(mainColor.style['background-color']== "lightgray"){
+		document.getElementById('main').style.backgroundColor = 'white';
+	}
+	else{
+		document.getElementById('main').style.backgroundColor = 'lightgray';
+	}
+
+	
+};
