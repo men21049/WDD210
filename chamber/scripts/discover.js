81c02d6239f1458d22f513a430e552b6abf8cc01
men21049 <men21049@byui.edu>
2024-02-28 23:49:15 -0600

    fix msg issue

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -12,15 +12,12 @@ let daysSinceLastVisit = (dateVisit - lastVisit)/msToDays;
 console.log(daysSinceLastVisit);
 if(daysSinceLastVisit == 0){
     welcome.textContent = "Welcome! Let us know if you have any questions."; 
-    console.log("Welcome! Let us know if you have any questions.");
 }
-else if(daysSinceLastVisit => 0 & daysSinceLastVisit <= 1){
+else if(daysSinceLastVisit => 0.001 & daysSinceLastVisit <= 1){
     welcome.textContent = "Back so soon! Awesome!"; 
-    console.log("Back so soon! Awesome!");
 }
 else{
     welcome.textContent = "You last visited n days ago.";
-    console.log("You last visited n days ago.");
 }
 
 
