5095bf79be56b3d62fc0ed6f91833064d614d22a
men21049 <men21049@byui.edu>
2024-03-21 18:36:24 -0600

    lesson 09

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,5 +1,6 @@
 const url = "https://men21049.github.io/wdd230/data/members.json";
-const card = document.querySelector('.card');
+const img_url = "https://men21049.github.io/wdd230/";
+const cardMember = document.querySelector('.card');
 
 window.onload = () =>{
 
@@ -10,9 +11,45 @@ window.onload = () =>{
       }
     
     function displayLinks(members){
-        members.array.forEach(member => {
-            console.log(member.names);    
+        members.forEach(member => {
+            const div = document.createElement('div');
+            const name = document.createElement('h4');
+            const img = document.createElement('img');
+            const address = document.createElement('p');
+            const phone = document.createElement('p');
+            const url = document.createElement('p');
+            const membership = document.createElement('p');
+            const other = document.createElement('p');
+
+            div.setAttribute('class','cardInfo');
+            name.textContent = member.names;
+
+            if(member.log = "None"){
+                img.setAttribute('src','https://placehold.co/40x40/png');
+            }
+            else{
+                img.setAttribute('src',img_url+member.logo);
+            }
+            div.appendChild(name);
+            div.appendChild(img);
+
         });
         
     }
+
+    getLinks();
 }
+/*
+<div class="cardInfo">
+                  <h4><b>Events</b></h4>
+                  <p>List of Events|</p>
+                </div>
+
+names":"Ciber papeleria misterpc",
+"addresses":"Artes Plásticas 273, Miravalle, 44990 Guadalajara, Jal.",
+"phone_numbers":"3327946713",
+"URL": "None",
+"logo":"images/directory/misterpc.png",
+"membership_level": "Silver Membership",
+"Other_Info"
+*/
