d0be84d9663478414690b1c37acb2ce629ecb6bc
Juan Carlos Mena <men21049@byui.edu>
2024-04-10 15:26:54 -0600

    fp

---
+++ FinalProject/scripts/CardImg.js
@@ -0,0 +1,29 @@
+const url = "https://men21049.github.io/wdd230/FinalProject/data/images.json";
+const img_url = "https://men21049.github.io/wdd230/FinalProject/";
+const imgage = document.getElementById('cozumelImg');
+const title = document.querySelector('.cozumelTitle')
+const description = document.querySelector('.cozumelDesc');
+
+window.onload = () =>{
+
+    async function getLinks() {
+        const response = await fetch(url);
+        const data = await response.json();
+        displaySpotlight(data.cozumel);
+      }
+    
+      function displaySpotlight(cozumel){
+        const min = 0;
+        const max = cozumel.length;
+        const idx = getRndInteger(min, max);
+
+        title.textContent = cozumel[idx].title;
+        description.textContent = cozumel[idx].description;
+      }
+
+      function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min) ) + min;
+      }
+
+    getLinks();
+}
