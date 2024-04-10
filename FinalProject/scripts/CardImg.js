3dcfc2e9cd2096d3b901ff8a35069d01c02d084f
Juan Carlos Mena <men21049@byui.edu>
2024-04-10 17:34:57 -0600

    FP

--- FinalProject/scripts/CardImg.js
+++ FinalProject/scripts/CardImg.js
@@ -1,6 +1,6 @@
 const url = "https://men21049.github.io/wdd230/FinalProject/data/images.json";
-const img_url = "https://men21049.github.io/wdd230/FinalProject/";
-const imgage = document.getElementById('cozumelImg');
+const img_url = "https://men21049.github.io/wdd230/FinalProject";
+const image = document.getElementById('cozumelImg');
 const title = document.querySelector('.cozumelTitle')
 const description = document.querySelector('.cozumelDesc');
 
@@ -19,6 +19,8 @@ window.onload = () =>{
 
         title.textContent = cozumel[idx].title;
         description.textContent = cozumel[idx].description;
+        image.setAttribute('src',img_url+ cozumel[idx].img);
+        image.setAttribute('srcset',img_url+ cozumel[idx].img);
       }
 
       function getRndInteger(min, max) {
